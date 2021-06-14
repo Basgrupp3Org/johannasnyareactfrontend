b0ee0df7f2ab5221d47a2d9dbdb2e46369480e2c
karrelo <kardo.rahim@hotmail.se>
2021-06-14 12:06:06 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import BudgetPage from './Components/Budget/BudgetPage'
+import HistoryPage from './Components/History/HistoryPage'
+import HomePage from './Components/Home/HomePage'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <Link to="/history">History</Link>
+              </li>
+              <li>
+                <Link to="/budget">Budget</Link>
+              </li>
+            </ul>
+          </nav>
+
+
+          <Switch>
+            <Route path="/history">
+              <HistoryPage />
+            </Route>
+            <Route path="/budget">
+              <BudgetPage />
+            </Route>
+            <Route path="/home">
+              <HomePage />
+            </Route>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
+    </>
   );
 }
 
---
+++ src/Components/History/HistoryPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HistoryPage() {
+    return (
+        <div>
+            <p>History</p>
+        </div>
+    )
+}
