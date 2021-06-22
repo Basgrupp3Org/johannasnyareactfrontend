659d1868f0c586e83ffda3e9d796c6a2e9cd68ea
omeramm <omer.amm11@gmail.com>
2021-06-22 14:50:57 +0200

    carlomerbudget

--- src/Components/Budget/Accordion.js
+++ src/Components/Budget/Accordion.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import chevron from "./chevron";
+
 import "./Accordion.css";
 
 function Accordion(props) {
@@ -23,19 +23,43 @@ function Accordion(props) {
         <div className="accordion__section">
             <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
                 <p className="accordion__title">{props.title}</p>
-                <cnhevron className={`${setRotate}`} width={10} fill={"#777"} />
+
             </button>
+
             <div
                 ref={content}
                 style={{ maxHeight: `${setHeight}` }}
                 className="accordion__content"
+
+
+
             >
+
+
+
+
+
                 <div
                     className="accordion__text"
+
                     dangerouslySetInnerHTML={{ __html: props.content }}
+
                 />
+
+                <button> hej </button>
+
+
+
+
+
+
+
+
+
             </div>
-        </div>
+        </div >
+
+
     );
 }
 
--- src/Components/Budget/BudgetPage.js
+++ src/Components/Budget/BudgetPage.js
@@ -10,35 +10,55 @@ function BudgetPage() {
         <div>
             <Accordion
                 title="Datum"
-                content="16-06-2021" />
+                content="16-06-2021"
+            />
 
             <Accordion
-                title="SparMål"
+                title="Sparmål"
                 content="lägg till "
 
+
             />
 
 
 
             <Accordion
-                title="Bostad"
-                content="100" />
+                title="Bostad      3000kr/mån"
+                content="3000:- Bosta" />
+
             <Accordion
-                title="Fordon"
-                content="1000" />
+                title="Fordon      3000kr/mån"
+                content="1000:-    Vovlo/Rejmes" />
+
             <Accordion
-                title="Rörliga kostnader"
-                content="500" />
+                title="Rörliga kostnader    8000kr"
+                content="500:-    SF Bio" />
+
             <Accordion
-                title="Fasta utgifter"
-                content="100" />
+                title="Fasta utgifter     1000kr/mån"
+                content="1000:-        CSN-Lån" />
+
             <Accordion
-                title="obudgeterat"
-                content="1000" />
+                title="Obudgeterat   1000kr/mån"
+                content="1000:-  Cykelaffär" />
+
             <Accordion
-                title="inkomst"
-                content="1000"
+                title="Inkomst   37000 SEK"
+                content="37000:-   Lön"
             />
+
+
+            <div clas="buttons">
+                <a class="btn1" >Lägg till Budget</a>
+                <a class="btn2">Lägg till Kategori</a>
+            </div>
+
+
+
+
+
+
+
 
 
 
